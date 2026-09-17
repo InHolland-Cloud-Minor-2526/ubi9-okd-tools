86a2f4ee46228a462f6f5d0e80e9cc6de629af68
Johan Kok <johankok@users.noreply.github.com>
2026-09-17 08:14:05 +0200

    Update tool versions in download-and-verify-tools.sh
    Updated versions for oc client and kubeconform in the script.

--- download-and-verify-tools.sh
+++ download-and-verify-tools.sh
@@ -3,7 +3,7 @@
 set -e
 
 # Download oc client
-VERSION="4.19.0-okd-scos.19"
+VERSION="4.22.0-okd-scos.10"
 BASE="https://github.com/okd-project/okd/releases/download/$VERSION/"
 
 for file in openshift-client-linux-amd64-rhel9-$VERSION.tar.gz sha256sum.txt sha256sum.txt.asc
@@ -21,7 +21,7 @@ sha256sum --check --ignore-missing sha256sum.txt
 tar zxf openshift-client-linux-amd64-rhel9-$VERSION.tar.gz && mv oc kubectl /usr/local/bin/
 
 # Download Kubeconform
-BASE="https://github.com/yannh/kubeconform/releases/download/v0.7.0"
+BASE="https://github.com/yannh/kubeconform/releases/download/v0.8.0"
 
 for file in CHECKSUMS kubeconform-linux-amd64.tar.gz
 do
